6d629f3dc39cf77598a6252ca0dc6c9f5a75c9c1
shakilahmedbhuiyan <16103107@iubat.edu>
2024-10-02 18:38:32 +0600

    gtag

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,6 +21,21 @@
     @vite('resources/css/app.css')
     @livewireStyles
     @stack('styles')
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-6MZMNHZ157"></script>
+    <script>
+        window.dataLayer = window.dataLayer || []
+
+        function gtag() {
+            dataLayer.push(arguments)
+        }
+
+        gtag('js', new Date())
+
+        gtag('config', 'G-6MZMNHZ157')
+    </script>
+
 </head>
 
 <body id="app" class="font-sans antialiased bg-gray-100 dark:bg-gray-900">
