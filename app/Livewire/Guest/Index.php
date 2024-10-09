54419a9680aa15f2e8bc0500a5b9a22d408efa32
shakilahmedbhuiyan <16103107@iubat.edu>
2024-10-09 23:26:45 +0600

    og image for index page

--- app/Livewire/Guest/Index.php
+++ app/Livewire/Guest/Index.php
@@ -16,8 +16,9 @@ class Index extends Component
         $description = 'Best Electronics offers all kinds of Smartphones with Installment facility all over Saudi Arabia. Choose your best device and gadget';
         $store = app('store');
         $this->seo()->setTitle('Best Mobile Shop in KSA');
+        $this->seo()->opengraph()->addImage(asset('assets/img/pay-later.png'));
         $this->seo()->metatags()->setKeywords(['Mobile Shop', 'iphone 16', 'Mobile Phones', 'Mobile Accessories']);
-        $this->seo()->opengraph()->setTitle('Best Mobile Shop in KSA- '.$store['name']);
+        $this->seo()->opengraph()->setTitle('Best Mobile Shop in KSA');
         $this->seo()->twitter()->setTitle($store['name']);
         $this->seo()->jsonLd()->setTitle('Best Mobile Shop in KSA- '.$store['name']);
         $this->seo()->setDescription('Best Mobile Shop in KSA- '.$description);
