1d6f800a2670d0107014d9cd09ce3710d797e328
shakilahmedbhuiyan <16103107@iubat.edu>
2024-10-21 00:20:06 +0600

    fix no brand device found for featured brand product

--- app/Livewire/Guest/Components/AppleDevices.php
+++ app/Livewire/Guest/Components/AppleDevices.php
@@ -10,36 +10,43 @@ use Livewire\Component;
 
 class AppleDevices extends Component
 {
-    public $devices;
+    public  $devices=[];
     public $brand;
     public $cache = false;
 
     public function mount($brand)
     {
-        $this->brand = cache::rememberForever('brand_' . $brand, function () use ($brand) {
-            return Brand::where('name', $brand)->first();
-        });
-
-        $key = $this->brand->slug . '_devices';
-
-        if (Cache::has($key)) {
-            $this->devices = Cache::get($key);
-            $this->cache = true;
+        if (Cache::has('brand_' . $brand)) {
+            $this->brand = Cache::get('brand_' . $brand);
         } else {
-            $this->devices = Product::where('brand_id', $this->brand->id)
-                ->where('status', true)
-                ->where('quantity', '>', 0)
-                ->orderBy('created_at', 'desc')
-                ->with('brand')
-                ->limit(4)
-                ->get();
-            Cache::forever($key,  $this->devices);
+            $this->brand = Brand::where('name', $brand)->first();
+            Cache::forever('brand_' . $brand, $this->brand);
         }
+      if ($this->brand) {
+
+          $key = $this->brand->slug . '_devices';
+
+          if (Cache::has($key)) {
+              $this->devices = Cache::get($key);
+              $this->cache = true;
+          } else {
+              $this->devices = Product::where('brand_id', $this->brand->id)
+                  ->where('status', true)
+                  ->where('quantity', '>', 0)
+                  ->orderBy('created_at', 'desc')
+                  ->with('brand')
+                  ->limit(4)
+                  ->get();
+              Cache::forever($key, $this->devices);
+          }
+      }
+
         return $this->devices;
     }
 
     public function render()
     {
+
         return view('livewire.guest.components.apple-devices');
     }
 }
